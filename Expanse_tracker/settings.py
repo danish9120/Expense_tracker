769c0592c2cd815ac83ed5c38937cc08a6c9132f
DANISH MARGOOB <KHANDANISHHMAD@GMAIL.COM>
2026-08-14 15:01:52 +0530

    Clean up settings.py by removing comments
    Removed commented sections for database, password validation, internationalization, and static files.

--- Expanse_tracker/settings.py
+++ Expanse_tracker/settings.py
@@ -57,10 +57,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Expanse_tracker.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -68,9 +64,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -88,9 +81,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -99,9 +89,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
