138f6774bb6973edbf5ed25dce583776a4b30b69
DANISH MARGOOB <KHANDANISHHMAD@GMAIL.COM>
2026-08-14 15:02:45 +0530

    Clean up blank lines in manage.py
    Removed unnecessary blank lines in manage.py.

--- manage.py
+++ manage.py
@@ -1,8 +1,5 @@
-
-
 import os
 import sys
-
 
 def main():
   
@@ -14,7 +11,6 @@ def main():
           
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
